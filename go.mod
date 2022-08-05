6eae73c13ddf53510fe99fe56f068b2f9f9f80d4
= <troy.a.varney@gmail.com>
2022-08-04 21:54:11 -0500

    Add flag parsing

--- go.mod
+++ go.mod
@@ -4,4 +4,12 @@ go 1.18
 
 require github.com/beevik/etree v1.1.0
 
-require github.com/tvarney/maputil v1.1.1
+require (
+	github.com/tvarney/maputil v1.1.1
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+)
--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
 	"github.com/tvarney/maputil/errctx"
+	"github.com/tvarney/sdtdmod/pkg/node"
 	"github.com/tvarney/sdtdmod/pkg/node/load"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 func main() {
@@ -15,23 +18,56 @@ func main() {
 }
 
 func run() int {
-	log.SetOutput(os.Stdout)
-	log.Printf("Loading ./config.json")
+	debug := kingpin.Flag("debug", "enable debug output").Short('D').Bool()
+	cnfgfile := kingpin.Flag("config", "path to the configuration file").Short('c').Default("./config.json").String()
 
-	n, err := load.LoadFile("config.json", &errctx.ErrorPrinter{Stream: os.Stderr})
+	_ = kingpin.Command("validate", "validate the configuration file")
+	apply := kingpin.Command("apply", "apply the configuration to the data")
+	applydir := apply.Arg("xmldir", "the directory containing XML files to update").Required().String()
+	dryrun := kingpin.Command("dry-run", "display changes that would be made")
+	dryrundir := dryrun.Arg("xmldir", "the directory continaing XML files to update").Required().String()
+
+	cmd := kingpin.Parse()
+	if *debug {
+		log.SetOutput(os.Stderr)
+	} else {
+		log.SetOutput(io.Discard)
+	}
+
+	log.Printf("Loading config file %q", *cnfgfile)
+	n, err := load.LoadFile(*cnfgfile, &errctx.ErrorPrinter{Stream: os.Stderr})
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error loading config: %v\n", err)
 		return 1
 	}
-	switch len(n) {
+
+	switch cmd {
+	case "validate":
+		return Validate(n)
+	case "apply":
+		return Apply(n, false, *applydir)
+	case "dry-run":
+		return Apply(n, true, *dryrundir)
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown command: %q", cmd)
+		return -1
+	}
+}
+
+func Apply(config []*node.Node, dryrun bool, dir string) int {
+	return 0
+}
+
+func Validate(config []*node.Node) int {
+	switch len(config) {
 	case 0:
 		fmt.Fprintf(os.Stdout, "No config nodes loaded\n")
 	case 1:
-		d, _ := json.MarshalIndent(n[0].Serialize(), "", "  ")
+		d, _ := json.MarshalIndent(config[0].Serialize(), "", "  ")
 		fmt.Fprintf(os.Stdout, "Config:\n%s\n", string(d))
 	default:
-		nodes := make([]map[string]interface{}, 0, len(n))
-		for _, node := range n {
+		nodes := make([]map[string]interface{}, 0, len(config))
+		for _, node := range config {
 			nodes = append(nodes, node.Serialize())
 		}
 		d, _ := json.MarshalIndent(nodes, "", "  ")
